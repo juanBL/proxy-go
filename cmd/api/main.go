bce54193cff57412f545f2f554b7a1b5e86a01fb
Juan Barrero Lopez <juan.barrero@meetingdoctors.com>
2022-10-19 17:18:31 +0200

    add gin framework

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
 )
@@ -11,13 +12,12 @@ const httpAddr = ":8080"
 func main() {
 	fmt.Println("Server running on", httpAddr)
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/health", healthHandler)
+	srv := gin.New()
+	srv.GET("/health", healthHandler)
 
-	log.Fatal(http.ListenAndServe(httpAddr, mux))
+	log.Fatal(srv.Run(httpAddr))
 }
 
-func healthHandler(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("everything is ok!"))
+func healthHandler(ctx *gin.Context) {
+	ctx.String(http.StatusOK, "everything is oks!")
 }
